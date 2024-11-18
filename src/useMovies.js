93914ef5b2622f3e05117ab15627015db990a25c
Atul Yadav <atuly3051@gmail.com>
2024-11-18 12:25:31 +0530

    Added

---
+++ src/useMovies.js
@@ -0,0 +1,41 @@
+import { useState,useEffect } from "react";
+
+export const useMovies = (query) => {
+    const [movies, setMovies] = useState([]);
+    const [isLodaing , setLoading] = useState(false);
+    const  [iserror, seterror] = useState("")
+    useEffect(function () {
+        async function moivefunctoion() {
+          try{
+            setLoading(true);
+            seterror("");
+            const response = await fetch(`http://www.omdbapi.com/?apikey=d855abd9&s=${query}`)
+            if(!response.ok) throw new Error("Something  wrong");
+            const  data = await response.json();
+           
+            if(data.Response === 'False') throw new Error("Something  wrong");
+            setMovies(data.Search);
+           
+           
+          }
+          catch(err){
+                console.error(err.message)
+                seterror(err.message)
+                setMovies([]);
+          }
+          finally{
+            setLoading(false);
+            
+          }
+        }
+        if(query.length<3)
+          {
+            seterror("")
+            setMovies([]);
+            return;
+          }
+        moivefunctoion();
+      }, [query]);
+
+     return [movies,iserror,isLodaing] 
+};
